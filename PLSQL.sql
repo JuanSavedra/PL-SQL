5bf2270fdf3a4b1ff8f64bb91fa4f6864d2070c7
JuanSavedra <juansavedra827@gmail.com>
2025-09-09 20:43:31 -0300

    if

--- PLSQL.sql
+++ PLSQL.sql
@@ -1,7 +1,15 @@
 SET SERVEROUTPUT ON;
 DECLARE
-    v_message varchar(40);
+    v_female boolean;
+    v_message varchar(70);
 BEGIN
-    v_message := 'Hello World'; /* Aspas simples devido ao varchar */
-    DBMS_OUTPUT.PUT_LINE ( v_message );
+    v_female := true;
+    
+    if v_female then 
+        v_message := 'É do sexo feminino.';
+    else
+        v_message := 'É do sexo masculino.';
+    end if;
+    
+    dbms_output.put_line(v_message);
 END;
