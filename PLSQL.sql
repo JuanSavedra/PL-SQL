f3eae1914c0a1aac415e109160e10e941c1a2fc4
JuanSavedra <juansavedra827@gmail.com>
2025-09-09 20:55:54 -0300

    subblocks

--- PLSQL.sql
+++ PLSQL.sql
@@ -1,15 +1,27 @@
 SET SERVEROUTPUT ON;
 DECLARE
-    v_female boolean;
-    v_message varchar(70);
+    v_female BOOLEAN;
+    v_message VARCHAR(70);
+    v_date_of_birth VARCHAR2(20) := to_char(to_date('15042006', 'ddmmyyyy'), 'day');
 BEGIN
-    v_female := true;
+    v_female := TRUE;
     
-    if v_female then 
-        v_message := 'É do sexo feminino.';
-    else
-        v_message := 'É do sexo masculino.';
-    end if;
+    IF v_female THEN
+        v_message := 'Feminino / ';
+    ELSE
+        v_message := 'Masculino / ';
+    END IF;
+    
+    -- SUB-BLOCO
+    DECLARE 
+        v_showComplete BOOLEAN := TRUE;
+        v_counter NUMBER := 0;
+    BEGIN
+        IF v_showComplete THEN
+            v_counter := v_counter + 1;
+            v_message := v_message || 'Nasceu em: ' || v_date_of_birth;
+        END IF;
+    END;
     
     dbms_output.put_line(v_message);
 END;
