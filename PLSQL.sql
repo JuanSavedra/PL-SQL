2302939b9a08a13d4e7fe8219cd36dbc1f3f0b67
JuanSavedra <juansavedra827@gmail.com>
2025-09-09 20:14:16 -0300

    introduction

--- PLSQL.sql
+++ PLSQL.sql
@@ -1,5 +1,7 @@
+SET SERVEROUTPUT ON;
 DECLARE
     v_message varchar(40);
 BEGIN
-    v_message := "Hello World!";
+    v_message := 'Hello World'; /* Aspas simples devido ao varchar */
+    DBMS_OUTPUT.PUT_LINE ( v_message );
 END;
