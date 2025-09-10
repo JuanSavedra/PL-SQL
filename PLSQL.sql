f1d8715de6ef95929eef542b2454c2dce374668b
JuanSavedra <juansavedra827@gmail.com>
2025-09-09 21:10:35 -0300

    constants

--- PLSQL.sql
+++ PLSQL.sql
@@ -1,27 +1,10 @@
-SET SERVEROUTPUT ON;
+SET SERVEROUTPUT ON
 DECLARE
-    v_female BOOLEAN;
-    v_message VARCHAR(70);
-    v_date_of_birth VARCHAR2(20) := to_char(to_date('15042006', 'ddmmyyyy'), 'day');
+    v_message VARCHAR(40) := 'PI: ' ;
+
+    -- Variável constante que não pode ser alterada
+    v_pi CONSTANT NUMBER(5, 4) := 3.1415;
 BEGIN
-    v_female := TRUE;
-    
-    IF v_female THEN
-        v_message := 'Feminino / ';
-    ELSE
-        v_message := 'Masculino / ';
-    END IF;
-    
-    -- SUB-BLOCO
-    DECLARE 
-        v_showComplete BOOLEAN := TRUE;
-        v_counter NUMBER := 0;
-    BEGIN
-        IF v_showComplete THEN
-            v_counter := v_counter + 1;
-            v_message := v_message || 'Nasceu em: ' || v_date_of_birth;
-        END IF;
-    END;
-    
-    dbms_output.put_line(v_message);
+    v_message := v_message || TO_CHAR(v_pi, '9D9999');
+    DBMS_OUTPUT.PUT_LINE(v_message);
 END;
