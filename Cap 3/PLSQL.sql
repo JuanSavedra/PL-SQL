6e8fbffa07882e18e881630f1e0be93ec0c00d54
JuanSavedra <juansavedra827@gmail.com>
2025-09-17 21:07:06 -0300

    %ROWTYPE

--- Cap 3/PLSQL.sql
+++ Cap 3/PLSQL.sql
@@ -1,6 +1,14 @@
 SET SERVEROUTPUT ON;
 DECLARE
-    v_nr_nota POSITIVEN NOT NULL := 9; --Positivo e não pode ser nulo
-BEGIN   
-    DBMS_OUTPUT.PUT_LINE(v_nr_nota);
+    rec_db_funcionario DB_FUNCIONARIO%ROWTYPE;
+BEGIN
+    SELECT *
+    INTO rec_db_funcionario
+    FROM DB_FUNCIONARIO
+    WHERE NR_MATRICULA_FUNC = 27;
+    
+    DBMS_OUTPUT.PUT_LINE('O funcionário ' || rec_db_funcionario.nm_funcionario 
+        || ' nasceu em ' || rec_db_funcionario.dt_nascimento
+        || ' e o seu status é ' || rec_db_funcionario.st_funcionario
+    );
 END;
